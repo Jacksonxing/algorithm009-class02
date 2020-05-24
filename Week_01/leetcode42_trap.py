e032535cf58c98091fb33f326da02efe47bb0d8c
Jacksonxing <zx227@cam.ac.uk>
2020-05-24 19:39:40 +0800

    update trap

--- Week_01/leetcode42_trap.py
+++ Week_01/leetcode42_trap.py
@@ -3,13 +3,10 @@ class Solution:
         stack = []
         water = 0
         for i, e in enumerate(height):
-            # we need to see if we can form a container
             while stack and e >= stack[-1][0]:
                 popped, _ = stack.pop()
-                # is it a container though? we have a left border?
                 if stack:
                     left_border, j = stack[-1]
-                    # we compute the water
                     water += min(left_border-popped, e-popped)*(i-j-1)
             stack.append((e,i))
         return water
